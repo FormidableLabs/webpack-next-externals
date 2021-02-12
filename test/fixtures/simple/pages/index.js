e0316ecb39d74fd4bcb5e463386fce21d8b04566
Ryan Roemer <ryan.roemer@formidable.com>
2021-02-12 14:50:15 -0800

    Get basic on-disk build going.

---
+++ test/fixtures/simple/pages/index.js
@@ -0,0 +1,12 @@
+
+export default function Home({}) {
+  return (
+    <div>
+      TODO: Real content.
+    </div>
+  )
+}
+
+// TODO: Add data.
+// export async function getStaticProps() {
+// }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,68 +1,14 @@
 "use strict";
 
-const fs = require("fs").promises;
 const path = require("path");
-const mockFs = require("mock-fs");
-const mockRequire = require('mock-require');
-const sinon = require("sinon");
-
-const nextRequire = require("next/dist/next-server/server/require");
-const { getPagePath } = nextRequire;
 const nextBuild = require("next/dist/build").default;
 
-const PAGES_MAP = {
-  "/_app": path.resolve(".next/pages/_app.js")
-};
-
 describe("TODO REMOVE", () => {
-  let sandbox;
 
-  beforeEach(async () => {
-    mockFs({});
-    sandbox = sinon.createSandbox();
-
-    sandbox
-      .stub(nextRequire, "getPagePath")
-      .callsFake((page, distDir, serverless, dev) => {
-        const found = PAGES_MAP[page];
-        if (found) { return found; }
-
-        return getPagePath(page, distDir, serverless, dev);
-      });
-  });
-
-  afterEach(() => {
-    mockFs.restore();
-    sandbox.restore();
-    mockRequire.stopAll();
-  });
-
-  it("TODO DO A NEXT COMPILE IN MEMORY", async () => {
-    mockFs({
-      pages: {
-        "_app.js": `
-          export default function App({ Component, pageProps }) {
-            return <Component {...pageProps} />
-          }
-        `,
-        "index.js": `
-          export default function Home() {
-            return (<h1>HI</h1>);
-          }
-        `
-      },
-      ".next": mockFs.load(path.resolve(__dirname, "../.next")),
-      "node_modules": mockFs.load(path.resolve(__dirname, "../node_modules"))
+  describe("test/fixtures/simple", () => {
+    it("TODO DO A NEXT COMPILE ON DISK :P", async () => {
+      const dir = path.resolve("test/fixtures/simple");
+      await nextBuild(dir);
     });
-
-    mockRequire(
-      path.resolve(".next/server/pages-manifest.json"),
-      {
-        "/": "pages/index.js",
-      }
-    );
-
-    const dir = process.cwd();
-    await nextBuild(dir);
   });
 });
