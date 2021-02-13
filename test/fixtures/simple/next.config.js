5d71a5cbef573d2d6f78849e01134fcce7f3b413
Ryan Roemer <ryan.roemer@formidable.com>
2021-02-12 16:09:01 -0800

    More debug info

--- test/fixtures/simple/next.config.js
+++ test/fixtures/simple/next.config.js
@@ -3,16 +3,26 @@
 module.exports = {
   target: "serverless",
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Add more information in the bundle.
-      config.output.pathinfo = true;
-    }
-
-    // Speed things up.
+    // Speed things up by disabling prod stuff we don't cate about.
     config.mode = "development";
     config.devtool = "false";
     config.optimization.minimize = false;
     config.optimization.minimizer = [];
+
+    if (isServer) {
+      // Add more information in the bundle.
+      config.output.pathinfo = true;
+
+      // TODO: HERE -- We care about these in terms of things to ignore.
+      console.log("TODO HERE CONFIG", {
+        resolve: {
+          alias: config.resolve.alias
+        },
+        resolveLoader: {
+          alias: config.resolveLoader.alias
+        }
+      });
+    }
 
     return config;
   }
